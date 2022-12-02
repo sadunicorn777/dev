58400c6556ce3c59b4e70662aa00662a2771c302
hadleydalt <hadley_dalton@brown.edu>
2022-12-02 11:52:57 -0500

    everything except sort

--- src/App.js
+++ src/App.js
@@ -9,13 +9,20 @@ data.forEach((item) => {
 });
 /* ############################################################## */
 
+function checkCategory(item, category) {
+  return item.category == category
+}
+
 
 function App() {
 
   const [cartItems, setCartItems] = useState([])
   const [total, setTotal] = useState(0)
+  const [category, setCategory] = useState("")
+  
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+
 
   return (
     <div className="App">
@@ -25,24 +32,25 @@ function App() {
         <h1>Filter and Sort</h1>
         <h2>❥ View by package</h2>
         <ul>
-          <button class ="side-panel-button"><h3>Zodiac Package</h3></button>
-          <button class ="side-panel-button"><h3>BTS Inspired Package</h3></button>
-          <button class ="side-panel-button"><h3>Euphoria Package</h3></button>
+          <button onClick={() => setCategory("zodiac")} class ="side-panel-button"><h3 style={category == "zodiac" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>Zodiac Package</h3></button>
+          <button onClick={() => setCategory("bts")} class ="side-panel-button"><h3 style={category == "bts" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>BTS Inspired Package</h3></button>
+          <button onClick={() => setCategory("euphoria")} class ="side-panel-button"><h3 style={category == "euphoria" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>Euphoria Package</h3></button>
         </ul>
         <h2>❥ View by aesthetic</h2>
         <ul>
-          <button class ="side-panel-button"><h3>E-girl/E-boy</h3></button>
-          <button class ="side-panel-button"><h3>Soft girl/Soft boy</h3></button>
-          <button class ="side-panel-button"><h3>Cottagecore</h3></button>
-          <button class ="side-panel-button"><h3>Artsy/Festive</h3></button>
-          <button class ="side-panel-button"><h3>Grunge/Goth/Macabre</h3></button>
-          <button class ="side-panel-button"><h3>Simple/Everyday/Baddie</h3></button>
+          <button onClick={() => setCategory("e")} class ="side-panel-button"><h3 style={category == "e" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>E-girl/E-boy</h3></button>
+          <button onClick={() => setCategory("soft")} class ="side-panel-button"><h3 style={category == "soft" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>Soft girl/Soft boy</h3></button>
+          <button onClick={() => setCategory("cottage")} class ="side-panel-button"><h3 style={category == "cottage" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>Cottagecore</h3></button>
+          <button onClick={() => setCategory("artsy")} class ="side-panel-button"><h3 style={category == "artsy" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>Artsy/Festive</h3></button>
+          <button onClick={() => setCategory("goth")} class ="side-panel-button"><h3 style={category == "goth" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>Grunge/Goth/Macabre</h3></button>
+          <button onClick={() => setCategory("simple")} class ="side-panel-button"><h3 style={category == "simple" ? {background: '#f194c7b1', color: 'white', filter: 'drop-shadow(0px 0px 4px #f194c7b1)'}: {}}>Simple/Everyday/Baddie</h3></button>
         </ul>
         <h2>❥ View all</h2>
 
 <div class="side-panel-sort-buttons">
         <button class="side-panel-sort">Sort by price</button>
         <button class="side-panel-sort">Sort by duration</button>
+        <button class="side-panel-sort">No sort</button>
         </div>
         </div>
       </div>
@@ -58,10 +66,55 @@ function App() {
       
 
       {data.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} onClick={() => {
+      category == "zodiac" ?
+        item.category == "zodiac" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
           setCartItems(cartItems => [...cartItems, item.name])
           setTotal(total + item.price)
-        }}/> // replace with BakeryItem component
+        }}/> : null // replace with BakeryItem component 
+        : category == "bts" ? 
+        item.category == "bts" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/> : null
+        : category == "euphoria" ? 
+        item.category == "euphoria" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/> : null
+        : category == "e" ? 
+        item.category == "e" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/> : null
+        : category == "soft" ? 
+        item.category == "soft" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/> : null
+        : category == "cottage" ? 
+        item.category == "cottage" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/> : null
+        : category == "artsy" ?
+        item.category == "artsy" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/> : null
+        : category == "goth" ? 
+        item.category == "goth" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/> : null
+        : category == "simple" ? 
+        item.category == "simple" ? <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/> : null
+        : <Item key = {index} image = {item.image} name = {item.name} description = {item.description} price = {item.price} duration = {item.duration} onClick={() => {
+          setCartItems(cartItems => [...cartItems, item.name])
+          setTotal(total + item.price)
+        }}/>
       ))}
 
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -15,8 +15,8 @@ export default function Item(props) {
            
         <div style={{marginLeft: "2vw"}}>
         <div style={{fontFamily: 'Courier New', fontWeight: 'bold'}} class = "side-panel-sort">{props.name}</div>
-        <p>{props.description}</p>
-        <p>${props.price} <button class="header-subtext" style={{fontWeight: 'bold'}} onClick={props.onClick}>Add To Cart</button></p>
+        <p style={{backgroundColor: '#f194c71d', padding: 5}}>{props.description}</p>
+        <p style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}> ⏳ {props.duration} hours 💸 ${props.price} <button class="header-subtext" style={{fontWeight: 'bold'}} onClick={props.onClick}>Add To Cart</button></p>
         
         </div>
         </div>
